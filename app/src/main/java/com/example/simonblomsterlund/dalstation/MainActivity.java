c5f0fc752191ff5f50f888d052079ca776fdb318
Simon Blomsterlund <simon@simonblund.com>
2017-06-15 16:58:29 +0300

    Basic splash startup done

--- app/src/main/java/com/example/simonblomsterlund/dalstation/MainActivity.java
+++ app/src/main/java/com/example/simonblomsterlund/dalstation/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.simonblomsterlund.dalstation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button openSetup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openSetup = (Button) findViewById(R.id.openSetup);
+        openSetup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "YouClicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
